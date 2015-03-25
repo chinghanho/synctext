4d9e509eb1ce0c1cd97275782047731c44d11426
CHH <chh@chh.tw>
2015-03-25 12:27:56 +0800

    first commit

---
+++ synctext.js
@@ -0,0 +1,42 @@
+(function ($) {
+
+  'use strict'
+
+  function Synctext(elem) {
+    this.elem = elem
+    this.$elem = $(elem)
+    this.target = this.$elem.data('target')
+    this.$target = $(this.target)
+    this.defaultText = this.$target.text()
+  }
+
+  Synctext.prototype.init = function () {
+    this.sync()
+    this.$elem.on('input', $.proxy(this.onInput, this))
+  }
+
+  Synctext.prototype.onInput = function (event) {
+    var key = event.keyCode || event.charCode // IE || others
+    if (key === 13) {
+      $(event.currentTarget).blur()
+      event.preventDefault() // prevent break line
+    }
+    this.sync()
+  }
+
+  Synctext.prototype.sync = function () {
+    var input = this.$elem.text()
+    if (input.length === 0) input = this.defaultText
+    this.$target.text(input)
+  }
+
+
+  $.fn.synctext = function () {
+    $(this).each(function (index, elem) {
+      new Synctext(elem).init()
+    })
+  }
+
+  $('[data-sync="text"]').synctext()
+
+})(jQuery)
